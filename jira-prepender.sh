e5d03598393ea3bb4030aea898c5fc0222c46503
Rupesh Parab <rupesh.parab@capitalfloat.com>
2018-03-30 15:34:40 +0530

    check if jira id is present in commit message

--- jira-prepender.sh
+++ jira-prepender.sh
@@ -4,9 +4,15 @@ set -e
 
 COMMIT_FILE=$1
 COMMIT_MSG=$(cat $1)
+JIRA_RE='[A-Za-z]{1,10}-[0-9]+'
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-JIRA_ID=$(echo "$CURRENT_BRANCH" | grep -Eo "[A-Z]{1,10}-[0-9]+")
-if [ ! -z "$JIRA_ID" ]; then
+JIRA_ID=$(echo "$CURRENT_BRANCH" | grep -Eo "$JIRA_RE" | awk '{print toupper($0)}')
+if [[ ! -z "$(echo $COMMIT_MSG | grep -E $JIRA_RE)" ]]; then
+    echo "JIRA ID present in current commit."
+elif [[ ! -z "$JIRA_ID" ]]; then
     echo "$JIRA_ID - $COMMIT_MSG" > $COMMIT_FILE
-    echo "JIRA ID '$JIRA_ID', matched in current branch name, prepended to commit message. (Use --no-verify to skip)"
+    echo "JIRA ID '$JIRA_ID', matched in current branch name, prepended to commit message."
+else
+    echo "JIRA ID not present in commit as well as branch"
+    exit 1
 fi
